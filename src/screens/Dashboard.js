8c651e28c4e52af9deb64ce44b05e1bd9bcd61ab
akoshti123 <88684554+akoshti123@users.noreply.github.com>
2022-12-01 14:10:16 -0600

    Add files via upload

---
+++ src/screens/Dashboard.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import Background from '../components/Background'
+import Logo from '../components/Logo'
+import Header from '../components/Header'
+import Paragraph from '../components/Paragraph'
+import Button from '../components/Button'
+
+export default function Dashboard({ navigation }) {
+  return (
+    <Background>
+      <Logo />
+      <Header>Success!</Header>
+      <Paragraph>
+        Logged in, Let's see what we have to do Today!
+      </Paragraph>
+      <Button
+        mode="outlined"
+        onPress={() =>
+          navigation.reset({
+            index: 0,
+            routes: [{ name: 'StartScreen' }],
+          })
+        }
+      >
+        Logout
+      </Button>
+    </Background>
+  )
+}
